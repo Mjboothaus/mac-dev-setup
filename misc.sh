31774ba7076b39f4c67fdf7340c826820253a326
Michael Booth <mjboothaus@Michaels-Laptop.local>
2022-12-05 23:54:57 +1100

    reformat

--- misc.sh
+++ misc.sh
@@ -4,7 +4,11 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
 
 echo "------------------------------"
 echo "Moving the Dock from bottom to left"
--- my_brew.sh
+++ my_brew.sh
@@ -5,24 +5,27 @@
 # Ask for the administrator password upfront.
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Keep-alive: update existing `sudo` time stamp until the script has
+finished.
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done
+2>/dev/null &
 
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+  /bin/bash -c "$(
+    curl -fsSL
+    https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+  )"
 fi
 
 # Make sure we’re using the latest Homebrew.
-# brew update
+brew update
 
 # Upgrade any already-installed formulae.
-# brew upgrade --all
-
-# Window manager
-brew install --cask rectangle
-
-# VS Code
-brew install --cask visual-studio-code
+brew upgrade --all
