e7920c80ca20b32eee9e1af3494ae242d36a052a
Michael J. Booth <mjboothaus@gmail.com>
2022-12-05 23:00:50 +1100

    Create misc.sh

---
+++ misc.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+echo "------------------------------"
+echo "Moving the Dock from bottom to left"
+defaults write com.apple.dock orientation left
+killall Dock
